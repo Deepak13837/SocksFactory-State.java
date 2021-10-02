e2309d220d62b134589ec00997bc4130fab3d4d3
Deepak13837 <90114760+Deepak13837@users.noreply.github.com>
2021-10-01 21:08:58 -0500

    Create PackingState.java

---
+++ PackingState.java
@@ -0,0 +1,51 @@
+package State;
+
+public class PackingState implements State{
+	SocksFactory socks;
+	 public PackingState(SocksFactory socks) {
+		// TODO Auto-generated constructor stub
+		 this.socks=socks;
+	}
+
+	@Override
+	public void knit() {
+		// TODO Auto-generated method stub
+		if(socks.checkWool()) {
+			System.out.println("Machine is turned on and Knitting is Started");
+			socks.setState(socks.getMachineOn());
+		}	else {
+			System.out.println("Wool is out of Stock....");
+			socks.setState(socks.getMachineOff());
+		}
+		
+		
+	}
+
+
+	@Override
+	public void Packing() {
+		// TODO Auto-generated method stub
+		System.out.println("Packing is completed");
+		
+	}
+
+
+	@Override
+	public void MachineOn() {
+		// TODO Auto-generated method stub
+		System.out.println("Machine is turned on");
+		
+	}
+
+
+	@Override
+	public void MachineOff() {
+		// TODO Auto-generated method stub
+		System.out.println("Machine is turned off");
+		
+	}
+	public String toString() {
+		return "Packing is started";
+	}
+
+}
